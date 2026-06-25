bc6c4814a614c098e0f69cfd561501e3dd3a1e87
Merci-Chi <bdzzsx8vrq@privaterelay.appleid.com>
2026-06-24 20:14:54 -0700

    Update copy-blocks-sw.js

--- copy-blocks-sw.js
+++ copy-blocks-sw.js
@@ -1,9 +1,20 @@
-const CACHE_NAME = "copy-blocks-v2";
+const CACHE_NAME = "copy-blocks-v3";
+const APP_PAGE = "./CopyBlocks.html";
 const APP_SHELL = [
-  "./CopyBlocks.html",
+  APP_PAGE,
   "./manifest.webmanifest",
-  "./dark-manifest.webmanifest"
+  "./dark-manifest.webmanifest",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png",
+  "./icons/dark-icon-192.png",
+  "./icons/dark-icon-512.png"
 ];
+
+function appShellResponse() {
+  return caches
+    .match(APP_PAGE, { ignoreSearch: true })
+    .then((response) => response || fetch(APP_PAGE));
+}
 
 self.addEventListener("install", (event) => {
   event.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(APP_SHELL)));
@@ -21,12 +32,30 @@ self.addEventListener("activate", (event) => {
 
 self.addEventListener("fetch", (event) => {
   if (event.request.method !== "GET") return;
+
+  const requestUrl = new URL(event.request.url);
+  const sameOrigin = requestUrl.origin === self.location.origin;
+
+  if (event.request.mode === "navigate" && sameOrigin) {
+    event.respondWith(
+      fetch(event.request)
+        .then((response) => {
+          if (response.ok) return response;
+          return appShellResponse();
+        })
+        .catch(() => appShellResponse()),
+    );
+    return;
+  }
+
   event.respondWith(
-    fetch(event.request).then((response) => {
-        if (response.ok && new URL(event.request.url).origin === self.location.origin) {
+    fetch(event.request)
+      .then((response) => {
+        if (response.ok && sameOrigin) {
           caches.open(CACHE_NAME).then((cache) => cache.put(event.request, response.clone()));
         }
         return response;
-      }).catch(() => caches.match(event.request))
+      })
+      .catch(() => caches.match(event.request, { ignoreSearch: true })),
   );
 });
